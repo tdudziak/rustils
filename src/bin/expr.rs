b6b9b955e8693cf74df998974ccfe361fe58886b
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2015-04-30 15:38:42 +0200

    Evaluation error handling in expr.

--- src/bin/expr.rs
+++ src/bin/expr.rs
@@ -1,21 +1,24 @@
 use std::env;
 use std::fmt;
+use std::error;
 
-use std::error::Error;
 use std::iter::Peekable;
 
 #[derive(Debug)]
-struct ParseError;
+enum Error { ParseError, EvalError }
 
-impl Error for ParseError {
+impl error::Error for Error {
     fn description(&self) -> &str {
-        "invalid expression"
+        match self {
+            &Error::ParseError => "invalid expression",
+            &Error::EvalError => "invalid value"
+        }
     }
 }
 
-impl fmt::Display for ParseError {
+impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        self.description().fmt(f)
+        (self as &error::Error).description().fmt(f)
     }
 }
 
@@ -33,30 +36,32 @@ enum Expr {
 }
 
 impl Expr {
-    fn eval_binop(op: BinOp, a: i64, b: i64) -> i64 {
+    fn eval_binop(op: BinOp, a: i64, b: i64) -> Result<i64, Error> {
         match op {
-            BinOp::Mul => a * b,
-            BinOp::Mod => a % b,
-            BinOp::Div => a / b,
-            BinOp::Add => a + b,
-            BinOp::Sub => a - b,
-            BinOp::Lower => (a < b) as i64,
-            BinOp::LowerEq => (a <= b) as i64,
-            BinOp::Equal => (a == b) as i64,
-            BinOp::NEqual => (a != b) as i64,
-            BinOp::Greater => (a > b) as i64,
-            BinOp::GreaterEq => (a >= b) as i64,
+            BinOp::Mul => Ok(a * b),
+            BinOp::Mod => Ok(a % b),
+            BinOp::Div => if b == 0 { Err(Error::EvalError) }
+                               else { Ok(a / b) },
+            BinOp::Add => Ok(a + b),
+            BinOp::Sub => Ok(a - b),
+            BinOp::Lower => Ok((a < b) as i64),
+            BinOp::LowerEq => Ok((a <= b) as i64),
+            BinOp::Equal => Ok((a == b) as i64),
+            BinOp::NEqual => Ok((a != b) as i64),
+            BinOp::Greater => Ok((a > b) as i64),
+            BinOp::GreaterEq => Ok((a >= b) as i64),
         }
     }
 
-    fn eval(&self) -> String {
+    fn eval(&self) -> Result<String, Error> {
         match self {
-            &Expr::Literal(ref x) => x.to_string(),
+            &Expr::Literal(ref x) => Ok(x.to_string()),
             &Expr::BinOp(op, ref ba, ref bb) => {
-                // FIXME: error handling
-                let val_a = ba.eval().parse().unwrap();
-                let val_b = bb.eval().parse().unwrap();
-                Expr::eval_binop(op, val_a, val_b).to_string()
+                let str_a = try!(ba.eval());
+                let val_a = try!(str_a.parse().map_err(|_| Error::EvalError));
+                let str_b = try!(bb.eval());
+                let val_b = try!(str_b.parse().map_err(|_| Error::EvalError));
+                Expr::eval_binop(op, val_a, val_b).map(|x| x.to_string())
             }
         }
     }
@@ -68,30 +73,30 @@ struct Parser<T> where T: Iterator<Item=String> {
 }
 
 impl <T> Parser<T> where T: Iterator<Item=String> {
-    fn parse_atom(&mut self) -> Result<Expr, ParseError> {
+    fn parse_atom(&mut self) -> Result<Expr, Error> {
         if self.tokens.peek() == Some(&"(".to_string()) {
             // try to parse a parenthesized subexpression
             self.tokens.next();
             let sub = try!(self.parse_expr());
             if self.tokens.next() != Some(")".to_string()) {
-                return Err(ParseError)
+                return Err(Error::ParseError)
             }
             Ok(sub)
         } else {
             let tok = match self.tokens.next() {
                 Some(x) => x,
-                None    => return Err(ParseError),
+                None    => return Err(Error::ParseError),
             };
 
             if tok.chars().all(|x| x.is_alphanumeric()) {
                 Ok(Expr::Literal(tok))
             } else {
-                Err(ParseError)
+                Err(Error::ParseError)
             }
         }
     }
 
-    fn parse_binop(&mut self, level: usize) -> Result<Expr, ParseError> {
+    fn parse_binop(&mut self, level: usize) -> Result<Expr, Error> {
         let sub_a: Expr =
             if level == 0 {
                 try!(self.parse_atom())
@@ -115,15 +120,15 @@ impl <T> Parser<T> where T: Iterator<Item=String> {
         Ok(Expr::BinOp(op, Box::new(sub_a), Box::new(sub_b)))
     }
 
-    fn parse_expr(&mut self) -> Result<Expr, ParseError> {
+    fn parse_expr(&mut self) -> Result<Expr, Error> {
         let idx = self.bin_ops.len() - 1;
         self.parse_binop(idx)
     }
 
-    fn parse(&mut self) -> Result<Expr, ParseError> {
+    fn parse(&mut self) -> Result<Expr, Error> {
         let expr = try!(self.parse_expr());
         match self.tokens.peek() {
-            Some(_) => Err(ParseError), // unused input
+            Some(_) => Err(Error::ParseError), // unused input
             None    => Ok(expr)
         }
     }
@@ -144,5 +149,5 @@ impl <T> Parser<T> where T: Iterator<Item=String> {
 
 fn main() {
     let expr = Parser::new(env::args().skip(1)).parse().unwrap();
-    println!("{}", expr, expr.eval());
+    println!("{}", expr.eval().unwrap());
 }
